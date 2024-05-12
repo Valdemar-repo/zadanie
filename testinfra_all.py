3c9785ee9dc21346260c9ea93577b20f4d27ac07
Valdemar <vladislavkachalo@gmail.com>
2024-05-13 01:03:20 +0300

    release

--- testinfra_all.py
+++ testinfra_all.py
@@ -3,6 +3,6 @@
 import pytest
 
 
-def test_nginx_on_port_443(host):
-    """check_nginx_port_443."""
-    assert host.socket.connect(('localhost', 443))
+def test_file_www_exists(host):
+    """Checking file var exists."""
+    assert host.file('/var/www/').exists
